4fbb44d34c4921f36fdd0e7c7debe28ed742f182
fil322 <filipchenko098@gmail.com>
2022-06-09 00:48:55 +0300

    first commit

---
+++ auth/users/models.py
@@ -0,0 +1,13 @@
+from enum import unique
+from msilib.schema import Class
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import CharField
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
